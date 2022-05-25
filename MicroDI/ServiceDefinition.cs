613b4fe44ee7a2186fd9e1ed3928ba65db60b285
Paul Brätz <paul.braetz@gmail.com>
2022-05-25 15:31:22 +0200

    overrode Tostring, Equals, GetHashCode

--- MicroDI/ServiceDefinition.cs
+++ MicroDI/ServiceDefinition.cs
@@ -7,9 +7,9 @@ using static MicroDI.IServiceDefinition;
 
 namespace MicroDI
 {
-	public readonly struct ServiceDefinition : IServiceDefinition
+	public readonly struct ServiceDefinition : IServiceDefinition, IEquatable<ServiceDefinition>
 	{
-		public ServiceDefinition(Type serviceType, Scope serviceScope, params Object[] parameters):this(serviceType, serviceType, serviceScope, parameters)
+		public ServiceDefinition(Type serviceType, Scope serviceScope, params Object[] parameters) : this(serviceType, serviceType, serviceScope, parameters)
 		{
 
 		}
@@ -25,5 +25,35 @@ namespace MicroDI
 		public readonly Type ServiceImplementation { get; }
 		public readonly Scope ServiceScope { get; }
 		public readonly IEnumerable<Object> ConstructorArguments { get; }
+
+		public override Boolean Equals(Object? obj)
+		{
+			return obj is ServiceDefinition definition && Equals(definition);
+		}
+
+		public Boolean Equals(ServiceDefinition other)
+		{
+			return ServiceType == other.ServiceType;
+		}
+
+		public override Int32 GetHashCode()
+		{
+			return HashCode.Combine(ServiceType);
+		}
+
+		public override String? ToString()
+		{
+			return $"{ServiceType.Name}->{ServiceImplementation.Name} [{ServiceScope}][{String.Join(", ", ConstructorArguments)}]";
+		}
+
+		public static Boolean operator ==(ServiceDefinition left, ServiceDefinition right)
+		{
+			return left.Equals(right);
+		}
+
+		public static Boolean operator !=(ServiceDefinition left, ServiceDefinition right)
+		{
+			return !(left == right);
+		}
 	}
 }
